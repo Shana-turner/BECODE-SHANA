70eb6aa84f89c2886f3bb53b70d9cf7d1944bbf7
Shana Biot <shanabiot.pro@gmail.com>
2024-05-06 09:21:40 +0200

    Dernier exercice OneLiner

--- WEEK-03-04-Algo/oneLiners.js
+++ WEEK-03-04-Algo/oneLiners.js
@@ -56,5 +56,55 @@ const randomRGB = () => `rgb(${Math.round(Math.random()*255)},${Math.round(Math.
 
 
 //// 08 ////
-const occurencies = (str, letter) => str.split('').includes(letter)
-console.log(occurencies("hello", "l"))
+const occurencies = (str, letter) => str.split('').filter((element) => element === letter).length
+
+/* console.log(occurencies("hello", "l"))  
+console.log(occurencies("abracadabra", "a"))
+console.log(occurencies("oups", "z")) */
+
+
+
+//// 09 ////
+const onlyPositives = (tabNumber) => tabNumber.map(num => num < 0  ? 0 : num).reduce((accumulator, currentValue) => accumulator + currentValue) 
+
+/* console.log(onlyPositives([1, 6, 2, -3, 5, -12]))
+console.log(onlyPositives([-3, -4, -2])) */
+
+
+//// 10 //// 
+const scanAndFind = (array, obj) => array.filter((element)=> Object.values(element)[1] === Object.values(obj)[0] )
+
+console.log(scanAndFind(
+    [
+        {
+            firstName: "Vito",
+            lastName: "Corleone",
+        },
+        {
+            firstName: "Jon",
+            lastName: "Snow",
+        },
+        {
+            firstName: "Harry",
+            lastName: "Potter",
+        },
+        {
+            firstName: "Michal",
+            lastName: "Corleone",
+        },
+    ],
+        {
+            lastName: "Corleone",
+        }
+)) // Result : [{firstName: Vito, lastName: Corleone}, {firstName: Michael, lastName: Corleone}]
+console.log(scanAndFind(
+    [
+    { fullName: "Roi Baudoin", id: 49762 },
+    { fullName: "Margareth Tatcher", id: 94357 },
+    { fullName: "Barack Obama", id: 76458 },
+    { fullName: "Emmanuel Macron", id: 10687 },
+    { fullName: "Charles de Gaulle", id: 67098 },
+    { fullName: "Boris Johnson", id: 16437 },
+    ],
+    { id: 10687 }
+))// Result : [{fullName: "Emmanuel Macron", id: 10687}]
