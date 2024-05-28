2ea4fe8be885497672eff1886b2add0328ba57fd
Shana Biot <shanabiot.pro@gmail.com>
2024-05-28 15:27:23 +0200

    ex-01 fini

---
+++ WEEK-7-DOM/Exercice08/Ex-01/app.js
@@ -0,0 +1,8 @@
+const btnHello = document.querySelector(".btn1");
+btnHello.addEventListener("click", ()=>{
+    alert("Oh my God ! You again !!!")
+})
+const btnGoodbye = document.querySelector(".btn2");
+btnGoodbye.addEventListener("click", ()=>{
+    alert("Thank God ! I thought you'd' never leave !")
+})
